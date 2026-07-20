ce280b3c5474f2f4374683139e44211ad32cd78d
L1nar <78925057+L1nar-dev@users.noreply.github.com>
2026-07-20 12:56:14 +0500

    Create WorldMusic.java

---
+++ src/main/java/ru/portal/worldmusic/WorldMusic.java
@@ -0,0 +1,51 @@
+package ru.portal.worldmusic;
+
+import de.maxhenkel.voicechat.api.VoicechatService;
+import org.bukkit.plugin.java.JavaPlugin;
+import java.io.File;
+
+public class WorldMusic extends JavaPlugin {
+    private static WorldMusic instance;
+    private MusicManager musicManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+
+        File tracksDir = new File(getDataFolder(), "tracks");
+        if (!tracksDir.exists()) {
+            tracksDir.mkdirs();
+        }
+
+        musicManager = new MusicManager();
+
+        // Регистрируем плагин в SVC API
+        VoicechatService service = getServer().getServicesManager().load(VoicechatService.class);
+        if (service != null) {
+            service.registerPlugin(new VoicechatPluginImpl());
+            getLogger().info("Успешное подключение к Simple Voice Chat API!");
+        } else {
+            getLogger().severe("Simple Voice Chat не найден! Плагин выключается.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        getCommand("worldmusic").setExecutor(new MusicCommand(this));
+    }
+
+    @Override
+    public void onDisable() {
+        if (musicManager != null) {
+            musicManager.shutdown();
+        }
+    }
+
+    public static WorldMusic getInstance() {
+        return instance;
+    }
+
+    public MusicManager getMusicManager() {
+        return musicManager;
+    }
+}
